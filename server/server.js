1600507ec120546552893ec9baeeebb0fc6ed728
Christopher Neisen <christopherjay71186@gmail.com>
2020-11-06 13:59:57 -0600

    trying to fix heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("server/public"));
 
-const creds = require('./client_secret.json')
+const creds = require('../client_secret.json')
 app.get("/progress", (req, res) => {
   function printReport(report) {
     let sku = report.sku;
