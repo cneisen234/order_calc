9d7e33c86db21dd4128808b00a979b653d4340d2
Christopher Neisen <christopherjay71186@gmail.com>
2020-11-12 13:36:54 -0600

    seems to be working, one more test

--- server/public/client.js
+++ server/public/client.js
@@ -23,10 +23,6 @@ function getProgress() {
   console.log("I am clicked")
   $("#progress").empty();
    $("#thisclears").empty();
-     $("#progress").append(
-       `<p>The import for Siser reporting has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
-    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
-     );
   $.ajax({
     type: "GET",
     url: "/progress",
@@ -36,8 +32,13 @@ function getProgress() {
       let timeout = Number(response)
       timeout *= 1000;
       console.log(timeout)
+           $("#progress").append(
+             `<p>The import for Siser reporting has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
+    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
+           );
   setTimeout(() => {
     console.log("timeout works", timeout)
+    endImport();
   }, timeout);
     }).catch(function (error) {
       //runs if post request fails
@@ -49,10 +50,6 @@ function getArtProgress() {
   console.log("I am clicked")
   $("#progress").empty();
    $("#thisclears").empty();
-     $("#progress").append(
-       `<p>The import for the art room has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
-    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
-     );
   $.ajax({
     type: "GET",
     url: "/art",
@@ -62,24 +59,24 @@ function getArtProgress() {
             let timeout = Number(response);
             timeout *= 1000;
             console.log(timeout);
+                 $("#progress").append(
+                   `<p>The import for the art room has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
+    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
+                 );
     setTimeout(() => {
       console.log("timeout works", timeout);
+      endImport();
     }, timeout);
     }).catch(function (error) {
       //runs if post request fails
       console.log("this is the error", error)
     });
-    // endImport();
 } // end getList
 
 function getThermProgress() {
   console.log("I am clicked")
   $("#progress").empty();
    $("#thisclears").empty();
-    $("#progress").append(
-      `<p>The import for the thermoflex plus has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
-    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
-    );
   $.ajax({
     type: "GET",
     url: "/therm",
@@ -89,8 +86,13 @@ function getThermProgress() {
             let timeout = Number(response);
             timeout *= 1000;
             console.log(timeout);
+               $("#progress").append(
+                 `<p>The import for the thermoflex plus has started, please refer to the spreadsheet <a href="https://docs.google.com/spreadsheets/d/1LSxm-aJNqi1tOGBkvG_Qmh1IkQGgKSCbdOu08TrltfI/edit?ts=5fa2ff6e#gid=0" target="_blank">here</a></br></br>
+    WARNING: Do not reload this page or start another import until the one currently running finishes</p>`
+               );
     setTimeout(() => {
       console.log("timeout works", timeout);
+      endImport();
     }, timeout);
     }).catch(function (error) {
       //runs if post request fails
